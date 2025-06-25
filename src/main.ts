87f7a1a0640201b4c43b1756102d52c76877ec4c
henzyd <henzydee@gmail.com>
2025-06-25 20:37:19 +0100

    config swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 
 const PORT = process.env.PORT ?? 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle("ResolveIt API")
+    .setDescription("This is the API docs for ResolveIt")
+    .setVersion("1.0")
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("docs", app, documentFactory);
   await app.listen(PORT);
+  console.log(`http://localhost:${PORT} - ${process.env.NODE_ENV}`);
 }
 void bootstrap();
