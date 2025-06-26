0835316380c68de52dbd39f3f2d999420ac1975c
henzyd <henzydee@gmail.com>
2025-06-26 18:27:43 +0100

    integrated db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { join } from "path";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 
 @Module({
-  imports: [ConfigModule.forRoot({ envFilePath: [".env.local", ".env"] })],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: [".env.local", ".env"] }),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      url: process.env.DATABASE_URL,
+      entities: [join(__dirname, "**", "*.entity.{ts,js}")],
+      synchronize: process.env.NODE_ENV !== "production",
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
